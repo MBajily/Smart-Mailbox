7c21bb8d65729c72c156550df2854bdce947d909
Mohammed Elgaily <MBajily@gmail.com>
2023-08-22 13:38:54 +0300

    2023-08-22

--- ekey/urls.py
+++ ekey/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('register/', views.register, name="register"),
-	path('locks/list/', views.locksList, name="locksList"),
-	path('passcode/random/', views.randomPasscode, name="randomPasscode"),
+	path('lock/list/', views.listLock, name="listLock"),
 	path('lock/<str:lock_id>/details/', views.lockDetails, name="lockDetails"),
+	path('<str:lock_id>/passcode/<str:type_id>/new/', views.getPasscode, name="getPasscode"),
+	path('<str:lock_id>/passcode/<str:passcode_id>/delete/', views.deletePasscode, name="deletePasscode"),
+	path('<str:lock_id>/passcode/list/', views.listPasscode, name="listPasscode"),
+	
 ]
