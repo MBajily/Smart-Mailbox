0b76a217f2f50cb0d2cae639efa07f41f233878a
Mohammed Elgaily <MBajily@gmail.com>
2024-01-16 14:56:54 +0300

    Add location and lockSound to lock list and lock details.

--- ekey/lock.py
+++ ekey/lock.py
@@ -38,18 +38,20 @@ def lockInit(request):
             date = round(time.time()*1000)
             data = json.loads(request.body.decode('utf-8'))
             lockData = data.get('lockData')
-            print("lockData =",lockData)
             payload = {'clientId':clientId, 'accessToken':auth_token, 'lockData':lockData, 'date':date}
             headers = {'Content-Type': 'application/x-www-form-urlencoded'}
             r = requests.post('https://cnapi.ttlock.com/v3/lock/initialize', headers=headers, params=payload)
         
             if "errcode" in (r.json()):
                 return HttpResponse(r, status=401)
+
+            print(r.json()["lockId"])
+            lockAutoLock(request, auth_token, lockId=r.json()["lockId"])
         
             return HttpResponse(r)
         
-        except:
-            return HttpResponse(status=401)
+        except Exception as e:
+            return HttpResponse(e, status=401)
 
     return HttpResponse(status=401)
 
@@ -70,14 +72,46 @@ def lockList(request):
         
         if "errcode" in (r.json()):
             return HttpResponse(status=401)
-        
-        return HttpResponse(r)
 
-    except:
-        redirect('logout')
-        return HttpResponse(status=401)
+        r = r.json()
+        result = {"list":[]}
+
+        for lock in r["list"]:
+            lockId = lock["lockId"]
+            selectedLocation = Location.objects.filter(lock_id=lockId).last()
+            if selectedLocation:
+                lock["location"] = {
+                    "latitude": float(selectedLocation.latitude),
+                    "longitude": float(selectedLocation.longitude)}
+            else:
+                lock["location"] = None
+
+            lock["lockSound"] = lockSound(request, auth_token, lockId)
+
+            result["list"].append(lock)
+
+
+        result = json.dumps(result)
+        
+        return HttpResponse(result)
+
+    except Exception as e:
+        return HttpResponse(e, status=401)
 
     return HttpResponse(status=401)
+
+
+def lockSound(request, auth_token, lockId):
+    if auth_token is None:
+        return HttpResponse(status=401)
+
+    # user = request.user
+    date = round(time.time()*1000)
+    payload = {'clientId':clientId, 'accessToken':auth_token, 'date':date, 'lockId':lockId}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    r = requests.get('https://cnapi.ttlock.com/v3/lock/detail', headers=headers, params=payload)
+    r = r.json()
+    return r["lockSound"]
 
 
 @csrf_exempt
@@ -98,9 +132,19 @@ def lockDetails(request):
         if "errcode" in (r.json()):
             return HttpResponse(status=401)
 
-        return HttpResponse(r)
+        r = r.json()
+        selectedLocation = Location.objects.filter(lock_id=lockId).last()
+        if selectedLocation:
+            r["location"] = {
+                "latitude": float(selectedLocation.latitude),
+                "longitude": float(selectedLocation.longitude)}
+        else:
+            r["location"] = None
 
-    except:
+        r = json.dumps(r)
+        return HttpResponse(r, content_type='application/json')
+
+    except Exception as e:
         return HttpResponse(status=401)
 
     return HttpResponse(status=401)
@@ -191,30 +235,22 @@ def lockSoundUpdate(request):
     return HttpResponse(status=401)
 
 
-@csrf_exempt
-def lockAutoLock(request):
-    if request.method == 'POST':
-        auth_token = request.META.get('HTTP_USER_TOKEN')
+
+def lockAutoLock(request, auth_token, lockId):        
+    if auth_token is None:
+        return HttpResponse("999999", status=401)
+
+    try:
+        date = round(time.time()*1000)
+
+        payload = {'clientId':clientId, 'accessToken':auth_token, 'lockId':lockId, 'seconds':5, 'type':1, 'date':date}
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        req = requests.post('https://euapi.ttlock.com/v3/lock/setAutoLockTime', headers=headers, params=payload)
         
-        if auth_token is None:
+        if (req.json())["errcode"] != 0:
             return HttpResponse(status=401)
+        print(req)
 
-        try:
-            date = round(time.time()*1000)
-            data = json.loads(request.body.decode('utf-8'))
-            lockId = data.get('lockId')
-            value = data.get('sound')
+    except Exception as e:
+        return HttpResponse(e, status=401)
 
-            payload = {'clientId':clientId, 'accessToken':auth_token, 'lockId':lockId, 'seconds':5, 'type':2, 'date':date}
-            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-            r = requests.post('https://euapi.ttlock.com/v3/lock/setAutoLockTime', headers=headers, params=payload)
-            
-            if (r.json())["errcode"] != 0:
-                return HttpResponse(status=401)
-
-            return HttpResponse(status=200)
-
-        except Exception as e:
-            return HttpResponse(status=401)
-
-    return HttpResponse(status=401)
